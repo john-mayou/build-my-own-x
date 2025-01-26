686e00e1809e757d49b7689c8ee37052277b0963
John Mayou <john@johnmayou.com>
2025-01-26 00:28:54 -0600

    add proof of work

--- ruby/blockchain/blockchain.rb
+++ ruby/blockchain/blockchain.rb
@@ -3,10 +3,23 @@ require 'digest'
 class Block
   attr_reader :data
   attr_reader :hash
+  attr_reader :nonce
 
   def initialize(data)
-    @data = data
-    @hash = Digest::SHA256.hexdigest(data)
+    @data         = data
+    @nonce, @hash = compute_hash_with_proof_of_work
+  end
+
+  def compute_hash_with_proof_of_work(difficulty = '00')
+    nonce = 0
+    loop do
+      hash = Digest::SHA256.hexdigest("#{nonce}#{data}")
+      if hash.start_with?(difficulty)
+        return [nonce, hash]
+      else
+        nonce += 1
+      end
+    end
   end
 end
 
